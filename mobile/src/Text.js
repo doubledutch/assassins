6b23e898f71f432f0d8cc4b1de79d3b3764b1eaa
Adam Liechty <adam@doubledutch.me>
2018-03-22 14:09:16 -0700

    Update font

--- mobile/src/Text.js
+++ mobile/src/Text.js
@@ -27,7 +27,7 @@ export default class Text extends PureComponent {
 const s = StyleSheet.create({
   text: {
     backgroundColor: 'transparent',
-    fontFamily: Platform.select({ios: 'AmericanTypewriter', android: 'Droid Sans'}),
+    fontFamily: Platform.select({ios: 'Menlo', android: 'Droid Sans'}),
     color: '#fff'
   }
 })
