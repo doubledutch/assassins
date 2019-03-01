d5682f2d2bc2bbe27e01235ce7e307758609b291
Adam Liechty <adam@doubledutch.me>
2019-02-28 16:04:04 -0800

    translations for arabic, german, spanish, french, italian, japanese, dutch

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,11 +1,30 @@
-import enUS from './en-us/assassins.mobile.json'
-// import deDE from './de-de/assassins.mobile.json'
+import enUS from './en-us/assassins.admin.json'
+
+import arINT from './ar-int/assassins.admin.json'
+import deDE from './de-de/assassins.admin.json'
+import esES from './es-es/assassins.admin.json'
+import frFR from './fr-fr/assassins.admin.json'
+import itIT from './it-it/assassins.admin.json'
+import jaJP from './ja-jp/assassins.admin.json'
+import nlNL from './nl-nl/assassins.admin.json'
 
 export default {
   '': enUS,
   en: enUS,
   'en-US': enUS,
 
-  // "de":    deDE,
-  // "de-DE": deDE,
+  ar: arINT,
+  'ar-INT': arINT,
+  de: deDE,
+  'de-DE': deDE,
+  es: esES,
+  'es-ES': esES,
+  fr: frFR,
+  'fr-FR': frFR,
+  it: itIT,
+  'it-IT': itIT,
+  ja: jaJP,
+  'ja-JP': jaJP,
+  nl: nlNL,
+  'nl-NL': nlNL,
 }
